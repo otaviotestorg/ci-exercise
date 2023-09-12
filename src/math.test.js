98a9f1ec1eeb8e5e892af9503493aabfe001625d
otavioomota <otaviomo19@gmail.com>
2023-09-12 20:26:52 -0300

    test: add func tests

--- src/math.test.js
+++ src/math.test.js
@@ -1,8 +1,28 @@
-const { sum } = require('./math');
+const { sum, sub, divison, exponent, main, times  } = require('./math');
 
 
 test("1 + 2 should be 3", () => {
-
     const total = sum(1,2);
     expect(total).toEqual(3);
 });
+
+test("5 - 2 should be 3", () => {
+    const total = sub(5,2);
+    expect(total).toEqual(3);
+});
+
+test("2 * 5 should be 10", () => {
+    const total = times(5,2);
+    expect(total).toEqual(10);
+});
+
+test("10 / 2 should be 5", () => {
+    const total = divison(10,2);
+    expect(total).toEqual(5);
+});
+
+test("2^4 should be 16", () => {
+    const total = exponent(2,4);
+    expect(total).toEqual(16);
+});
+
